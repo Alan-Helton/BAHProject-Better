21ee0c241674d9a8804d02d8bd847705602442a6
user <user@this.com>
2019-11-06 14:53:00 -0500

    master commit

---
+++ MCCProject/src/main/java/com/mcc/repository/CustomersRepository.java
@@ -0,0 +1,9 @@
+package com.mcc.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mcc.domain.Customer;
+
+public interface CustomersRepository extends CrudRepository<Customer, Long> {
+
+}
---
+++ MCCProject/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into CUSTOMERS (CUSTOMER_NAME, EMAIL, PASSWORD) VALUES ('Bruce', 'bruce@a.com', 'abc123');
+insert into CUSTOMERS (CUSTOMER_NAME, EMAIL, PASSWORD) VALUES ('Paul', 'paul@b.com', '123ABC');
+insert into CUSTOMERS (CUSTOMER_NAME, EMAIL, PASSWORD) VALUES ('Rick', 'rick@c.com', '123XYZ');
