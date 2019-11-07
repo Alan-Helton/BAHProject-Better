e69c77f243b328fff2f8584bb1547f5617538fbd
user <user@this.com>
2019-11-07 16:13:51 -0500

    Proper modification

---
+++ MCCProject/src/main/java/com/mcc/repository/CustomersRepository.java
@@ -0,0 +1,13 @@
+package com.mcc.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mcc.domain.Customer;
+
+public interface CustomersRepository extends CrudRepository<Customer, Long> {
+
+	public Customer findByName(String name);
+	
+	public Customer deleteByName(String name);
+	
+}
