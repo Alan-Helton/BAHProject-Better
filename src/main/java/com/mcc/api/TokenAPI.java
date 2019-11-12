03addbb86b6cab92a29ca0200c7437d270b733b6
Ragnakor <berebere101@gmail.com>
2019-11-12 15:58:57 -0500

    Minor fixes for Token.

--- src/main/java/com/mcc/api/TokenAPI.java
+++ src/main/java/com/mcc/api/TokenAPI.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.api.Authenticator;
+//import com.example.api.Authenticator;
 import com.mcc.util.JWTHelper;
 
-import com.example.api.Token;
-import com.example.api.TokenRequestData;
+import com.mcc.domain.Token;
+//import com.example.api.TokenRequestData;
 
 public class TokenAPI {
 		JWTUtil jwtUtil = new JWTHelper();
